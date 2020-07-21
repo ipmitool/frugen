5dc1fd9a03b9c26ab7feb75d631f091b447943b2
Aneesh Durg <aneeshdurg17@gmail.com>
2020-07-21 15:25:30 -0700

    Retab

--- fru_reader.c
+++ fru_reader.c
@@ -5,55 +5,55 @@
 #include "fatal.h"
 
 void safe_read(int fd, void *buffer, size_t length) {
-    size_t total_bytes_read = 0;
-    while (total_bytes_read != length) {
-        ssize_t bytes_read = read(
+	size_t total_bytes_read = 0;
+	while (total_bytes_read != length) {
+		ssize_t bytes_read = read(
 			fd, buffer + total_bytes_read, length - total_bytes_read);
-        if (bytes_read == -1)
-            fatal("Error reading file");
+		if (bytes_read == -1)
+			fatal("Error reading file");
 
-        total_bytes_read += bytes_read;
-    }
+		total_bytes_read += bytes_read;
+	}
 }
 
 /**
  * Allocate and read a fru_chassis_area_t from a file descriptor
  */
 fru_chassis_area_t *read_fru_chassis_area(int fd) {
-    size_t base_len = sizeof(fru_chassis_area_t);
-    fru_chassis_area_t *area = malloc(base_len);
-    safe_read(fd, area, base_len);
-    size_t data_len = 8 * area->blocks;
-    area = realloc(area, base_len + data_len);
-    safe_read(fd, &area->data, data_len);
+	size_t base_len = sizeof(fru_chassis_area_t);
+	fru_chassis_area_t *area = malloc(base_len);
+	safe_read(fd, area, base_len);
+	size_t data_len = 8 * area->blocks;
+	area = realloc(area, base_len + data_len);
+	safe_read(fd, &area->data, data_len);
 
-    return area;
+	return area;
 }
 
 /**
  * Allocate and read a fru_board_area_t from a file descriptor
  */
 fru_board_area_t *read_fru_board_area(int fd) {
-    size_t base_len = sizeof(fru_board_area_t);
-    fru_board_area_t *area = malloc(base_len);
-    safe_read(fd, area, base_len);
-    size_t data_len = 8 * area->blocks;
-    area = realloc(area, base_len + data_len);
-    safe_read(fd, &area->data, data_len);
+	size_t base_len = sizeof(fru_board_area_t);
+	fru_board_area_t *area = malloc(base_len);
+	safe_read(fd, area, base_len);
+	size_t data_len = 8 * area->blocks;
+	area = realloc(area, base_len + data_len);
+	safe_read(fd, &area->data, data_len);
 
-    return area;
+	return area;
 }
 
 /**
  * Allocate and read a fru_product_area_t from a file descriptor
  */
 fru_product_area_t *read_fru_product_area(int fd) {
-    size_t base_len = sizeof(fru_product_area_t);
-    fru_product_area_t *area = malloc(base_len);
-    safe_read(fd, area, base_len);
-    size_t data_len = 8 * area->blocks;
-    area = realloc(area, base_len + data_len);
-    safe_read(fd, &area->data, data_len);
+	size_t base_len = sizeof(fru_product_area_t);
+	fru_product_area_t *area = malloc(base_len);
+	safe_read(fd, area, base_len);
+	size_t data_len = 8 * area->blocks;
+	area = realloc(area, base_len + data_len);
+	safe_read(fd, &area->data, data_len);
 
-    return area;
+	return area;
 }
