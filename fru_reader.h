dea002c5e5ff499a961a8c30e2bb4aaf16a821c2
Aneesh Durg <aneeshdurg17@gmail.com>
2020-07-21 15:52:27 -0700

    Read fru header into struct and check for errors

--- fru_reader.h
+++ fru_reader.h
@@ -1,5 +1,5 @@
 #include "fru.h"
-void safe_read(int fd, void *buffer, size_t length);
+fru_t *read_fru_header(int fd);
 fru_chassis_area_t *read_fru_chassis_area(int fd);
 fru_board_area_t *read_fru_board_area(int fd);
 fru_product_area_t *read_fru_product_area(int fd);
